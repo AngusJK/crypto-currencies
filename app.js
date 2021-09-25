487bc318f1358b8d6de776e28c9061289778dc36
Angus Kellett <anguskellett@hotmail.com>
2021-09-25 01:55:54 +0000

    creates fetch method

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 const baseUrl = "https://api.coinranking.com/v2/coins";
-const proxyUrl = "https://cors-anywhere.heroku.com"
-const apiKey = "coinranking14378675bb7d51f3f806e64d4dde51dfeda2dbc323d33e13"
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const apiKey = "coinranking14378675bb7d51f3f806e64d4dde51dfeda2dbc323d33e13";
+
+fetch(`${proxyUrl}${baseUrl}`, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+    "x-access-token": `${apiKey}`,
+    "Access-Control-Allow-Origin": "*"
+  }
+}).then((response) => {
+  if(response.ok) {
+    response.json().then((json) => {
+      console.log(json)
+    })
+  }
+}).catch((error) => {
+  console.log(error);
+});
 
