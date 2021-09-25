c4d2b201b8ef6cff13564ba3ced6489e1eab199d
Angus Kellett <anguskellett@hotmail.com>
2021-09-25 01:31:05 +0000

    adds baseUrl, proxyUrl, and apiKey

---
+++ app.js
@@ -0,0 +1,4 @@
+const baseUrl = "https://api.coinranking.com/v2/coins";
+const proxyUrl = "https://cors-anywhere.heroku.com"
+const apiKey = "coinranking14378675bb7d51f3f806e64d4dde51dfeda2dbc323d33e13"
+
