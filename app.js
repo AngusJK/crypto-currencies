0843c09f07684c728816170a4c0315e606be7e2d
Angus Kellett <anguskellett@hotmail.com>
2021-09-27 22:58:13 +0000

    creates coinsData variable and creates table with forEach method

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ const apiKey = "coinranking14378675bb7d51f3f806e64d4dde51dfeda2dbc323d33e13";
 fetch(`${proxyUrl}${baseUrl}`, {
   method: "GET",
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": 'application/json',
     "x-access-token": `${apiKey}`,
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": '*'
   }
 }).then((response) => {
   if(response.ok) {
     response.json().then((json) => {
       console.log(json)
+
+      let coinsData = json.data.coins
+
+      if (coinsData.length >0 ) {
+        var cryptoCoins = ""
+      }
+
+      coinsData.forEach((coins) => {
+        cryptoCoins += "<tr>";
+        cryptoCoins += `<td> ${coin.uuid} </td>`;
+      })
     })
   }
 }).catch((error) => {
